65f1a62fca934d0c8ba38fa3935d0e3718a2be6a
AMaldonado1 <a100maldo983@outlook.com>
2024-08-06 13:57:24 -0400

    Descriptive Statistical Analysis

--- main.R
+++ main.R
@@ -34,3 +34,33 @@ max_months_since <- dbGetQuery(connection, "SELECT MAX(months_since_first_credit
 avg_months_since <- dbGetQuery(connection, "SELECT AVG(months_since_first_credit) FROM lending;")
 dbDisconnect(connection)
 
+# Descriptive Statistical Analysis 
+
+# Descriptive Statistics of the 3 field columns with queries and double check in R
+# Gets the Minimum, Median, Mean, and Maximum of the loan amount
+print(min_loan_amnt)
+min(three_parts_data$loan_amnt)
+print(max_loan_amnt)
+max(three_parts_data$loan_amnt)
+print(avg_loan_amnt)
+mean(three_parts_data$loan_amnt)
+sd(three_parts_data$loan_amnt)
+
+# Gets the Minimum, Median, Mean, and Maximum of the adjusted annual income
+print(min_annual_income)
+min(three_parts_data$adjusted_annual_inc)
+print(max_annual_income)
+max(three_parts_data$adjusted_annual_inc)
+print(avg_annual_income)
+mean(three_parts_data$adjusted_annual_inc)
+sd(three_parts_data$adjusted_annual_inc)
+
+# Gets the Minimum, Median, Mean, and Maximum of the months since first credit
+print(min_months_since)
+min(three_parts_data$months_since_first_credit)
+print(max_months_since)
+max(three_parts_data$months_since_first_credit)
+print(avg_months_since)
+mean(three_parts_data$months_since_first_credit)
+sd(three_parts_data$months_since_first_credit)
+
